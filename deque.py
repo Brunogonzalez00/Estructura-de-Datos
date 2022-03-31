f4f5d3ac63b6affb7a27dd58063168ab6474aaa9
Brunogonzalez00 <99372285+Brunogonzalez00@users.noreply.github.com>
2022-03-30 21:14:10 -0400

    Add files via upload

---
+++ deque.py
@@ -0,0 +1,18 @@
+from collections import deque
+
+numbers= deque()
+
+numbers.append(99)
+numbers.append(15)
+numbers.append(82)
+numbers.append(50)
+numbers.append(47)
+
+last_item = numbers.pop()
+print(last_item) 
+print(numbers) 
+
+
+first_item = numbers.popleft()
+print(first_item) 
+print(numbers) 
